d0392cc84303abdd749b36658929097168137c24
KotovskyS <sokotovskiy@edu.hse.ru>
2023-05-04 17:11:01 +0300

    Fixed Auth error. Added README

---
+++ README.md
@@ -0,0 +1,52 @@
+# Puppeteer Ejudge API
+
+This project provides an API to interact with the Ejudge platform using Puppeteer for web scraping. The main functionality includes user authentication, handling solutions, getting results, and parsing tasks.
+Prerequisites
+
+To run this project, you need to have Node.js and npm installed on your machine.
+Installation
+
+### Clone the repository:
+
+```
+git clone https://github.com/Task-Testing-System-Devs/test-system-handler.git
+```
+
+### Go to the project directory:
+
+```
+cd test-system-handler
+```
+
+### Install the required dependencies:
+
+```
+npm install
+```
+
+### Usage
+
+#### Start the server:
+
+```
+node puppeteerApi.js
+```
+
+    The API will be running at http://localhost:3000.
+
+    Use the following endpoints to interact with the Ejudge platform:
+
+        POST /auth: Authenticate a user. Requires a JSON body with login, password, and contestID.
+
+        POST /handleSolution: Handle a solution for a specific task. Requires a JSON body with solutionFileBase64 (base64-encoded solution file) and taskID.
+
+        GET /getResult: Retrieve the result of the last submitted solution.
+
+        GET /parseTasks: Retrieve a list of tasks from the current contest.
+
+### Rate Limiting
+
+The server has rate limiting implemented to protect against abuse (DDoS). It allows a maximum of 300 requests within a 5-minute window.
+### Error Handling
+
+The server has basic error handling implemented. If an error occurs while processing a request, it will return a 500 status code with an error message.
--- puppeteer.js
+++ puppeteer.js
@@ -7,7 +7,6 @@ const { createPool } = require('generic-pool');
 let currentUrl;
 let functionUrl;
 
-// Create a browser pool
 const browserPool = createPool({
     create: async () => {
         const browser = await puppeteer.launch({ headless: true });
@@ -74,26 +73,41 @@ async function getPage(browser) {
 }
 
 async function auth(ejudgeLogin, ejudgePassword, contestID) {
-    await withPage(async (page) => {
-        try {
-            await page.goto('http://37.252.0.155/cgi-bin/master');
-            await page.waitForSelector('input[name=login]', { timeout: 3000 });
-            await page.waitForSelector('input[name=password]', { timeout: 3000 });
-            await page.waitForSelector('input[name=contest_id]', { timeout: 3000 });
-            await page.waitForSelector('select[name=role]', { timeout: 3000 });
-            await page.waitForSelector('input[name=action_2]', { timeout: 3000 });
-            await page.$eval('input[name=login]', function (el, value) { el.value = value }, ejudgeLogin);
-            await page.$eval('input[name=password]', function (el, value) { el.value = value }, ejudgePassword);
-            await page.$eval('input[name=contest_id]', function (el, value) { el.value = value }, contestID);
-            await page.$eval('select[name=role]', el => el.value = '6');
-            await page.click('input[name=action_2]');
-
-            currentUrl = page.url();
-            console.log(currentUrl);
-        } catch (error) {
-            console.error(error);
-        }
-    });
+    const maxRetries = 3;
+    let retries = 0;
+    let authenticated = false;
+    while (retries < maxRetries && !authenticated) {
+        await withPage(async (page) => {
+            try {
+                await page.goto('http://37.252.0.155/cgi-bin/master');
+                await page.waitForSelector('input[name=login]', { timeout: 3000 });
+                await page.waitForSelector('input[name=password]', { timeout: 3000 });
+                await page.waitForSelector('input[name=contest_id]', { timeout: 3000 });
+                await page.waitForSelector('select[name=role]', { timeout: 3000 });
+                await page.waitForSelector('input[name=action_2]', { timeout: 3000 });
+                await page.$eval('input[name=login]', (el, value) => { el.value = value }, ejudgeLogin);
+                await page.$eval('input[name=password]', (el, value) => { el.value = value }, ejudgePassword);
+                await page.$eval('input[name=contest_id]', (el, value) => { el.value = value }, contestID);
+                await page.$eval('select[name=role]', el => el.value = '6');
+                await page.click('input[name=action_2]');
+                currentUrl = page.url();
+                console.log(currentUrl);
+                if (currentUrl.includes('SID')) {
+                    authenticated = true;
+                } else {
+                    retries++;
+                    console.log(`Retrying authentication (${retries}/${maxRetries})`);
+                }
+            } catch (error) {
+                console.error(error);
+                retries++;
+                console.log(`Retrying authentication due to error (${retries}/${maxRetries})`);
+            }
+        });
+    }
+    if (retries >= maxRetries && !authenticated) {
+        throw new Error('Failed to authenticate after multiple retries');
+    }
 }
 
 async function handleSolution(solutionFileBase64, taskID) {
